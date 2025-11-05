b4b98b3c40c81ca8101d414b848b26548261207b
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-05 19:54:41 +0100

    Build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,17 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: number;
+  location: string;
+  contract: boolean;
+}
+
+const teacher1: Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: false,
+  yearsOfExperience: 3,
+  location: "London",
+  contract: false,
+};
