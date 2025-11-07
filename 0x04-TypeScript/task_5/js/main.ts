f7aef9b37aea8291ccf240673adf6696cece3e03
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-07 09:58:31 +0100

    Brand convention & nominal typing

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,16 @@
+interface MajorCredits {
+  credits: number;
+}
+
+interface MinorCredits {
+  credits: number;
+}
+
+function sumMajorCredits(subject1: "MajorCredits" + "MinorCredits"){
+return .MajorCredits
+}
+
+function sumMinorCredits(subject2: "MajorCredits" + "MinorCredits"){
+
+return .MinorCredits
+}
